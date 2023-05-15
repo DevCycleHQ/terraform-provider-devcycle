91481812d240b9f9b4465aefd232fe3001b35fa1
Jamie Sinn <jamie.sinn@taplytics.com>
2023-05-15 15:04:36 -0400

    Add test case and re-tidy imports

--- internal/provider/evaluated_boolean_variable_data_source_test.go
+++ internal/provider/evaluated_boolean_variable_data_source_test.go
@@ -20,11 +20,31 @@ func TestAccEvaluatedBooleanFeatureDataSource(t *testing.T) {
 			},
 		},
 	})
+	resource.Test(t, resource.TestCase{
+		PreCheck:                 func() { testAccPreCheck(t) },
+		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
+		Steps: []resource.TestStep{
+			// Read testing
+			{
+				Config: testAccEvaluatedBoolVariableDataSourceConfig,
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr("data.devcycle_evaluated_variable_boolean.test", "value", "false"),
+				),
+			},
+		},
+	})
 }
 
 const testAccEvaluatedBoolVariableDataSourceConfig = `
 data "devcycle_evaluated_variable_boolean" "test" {
   id = "acceptance-testing-boolean"
+  user = {
+	id = "acceptancetesting"
+  }
+  default_value = false
+}
+data "devcycle_evaluated_variable_boolean" "test-default" {
+  id = "acceptance-testing-boolean-no-exist"
   user = {
 	id = "acceptancetesting"
   }
